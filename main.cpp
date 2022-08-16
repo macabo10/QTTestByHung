857344c47dded1f0b82dce6aa668d77472b8fdd9
macabo10 <macabo10quymui@gmail.com>
2022-08-16 16:37:27 +0700

    4th  commit

--- main.cpp
+++ main.cpp
@@ -21,15 +21,13 @@ void testQBEInt(QBEInteger<int> *ptr) {
 int main(int argc, char **argv)
 {
     QApplication app (argc, argv);
-//    window *win = new window();
-//    win->show();
-//    window win;
-//    win.show();
-//    int n = 10;
-//    QBEInteger<int> *ptr = new QBEInteger<int>;
-//    ptr->operator=(n);
-//    cout << ptr->operator%=(3) << endl;
-//    testQBEInt(ptr);
+    window *win = new window();
+    win->show();
+    int n = 10;
+    QBEInteger<int> *ptr = new QBEInteger<int>;
+    ptr->operator=(n);
+    cout << ptr->operator%=(3) << endl;
+    testQBEInt(ptr);
     QueueTest qt = QueueTest(5);
     qt.add();
     qt.pull();
